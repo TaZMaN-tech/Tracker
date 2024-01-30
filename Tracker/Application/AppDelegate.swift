abd5ba49252a8d0a48b0a89b79c21404b1f93f79
Tadevos Kurdoglian <tazman311@gmail.com>
2024-01-30 13:05:12 +0700

    fix missed files

--- Tracker/Application/AppDelegate.swift
+++ Tracker/Application/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  Tracker
-//
-//  Created by Тадевос Курдоглян on 13.09.2023.
-//
-
 import UIKit
 
 @main
@@ -13,7 +6,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor.ypWhite
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.ypBlack ?? UIColor.black]
+        UINavigationBarAppearance().titleTextAttributes = [.font: UIFont.ypMediumSize16]
+        UINavigationBar.appearance().prefersLargeTitles = false
+        UINavigationBar.appearance().backgroundColor = UIColor.ypWhite
         return true
     }
 
